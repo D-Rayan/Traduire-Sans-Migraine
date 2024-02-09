3dbaee93b36b56082b3890b0c2acebbf79e8c6ac
Rayan <rayan.djebli@hotmail.fr>
2024-02-09 15:51:01 +0100

    trying fix translation

--- includes/Wordpress/TextDomain.php
+++ includes/Wordpress/TextDomain.php
@@ -6,12 +6,12 @@ class TextDomain
 {
     public function loadTextDomain()
     {
-        load_plugin_textdomain(TSM__TEXT_DOMAIN, false, TSM__ABSOLUTE_PATH . '/languages');
+        load_plugin_textdomain("traduire-sans-migraine", false, TSM__ABSOLUTE_PATH . '/languages');
         add_filter( 'load_textdomain_mofile', [$this, "loadMOFile"], 10, 2 );
     }
 
     function loadMOFile( $mofile, $domain ) {
-        if ( TSM__TEXT_DOMAIN === $domain && false !== strpos( $mofile, WP_LANG_DIR . '/plugins/' ) ) {
+        if ( "traduire-sans-migraine" === $domain && false !== strpos( $mofile, WP_LANG_DIR . '/plugins/' ) ) {
             $locale = apply_filters( 'plugin_locale', determine_locale(), $domain );
             $mofile = WP_PLUGIN_DIR . '/' . TSM__PLUGIN_NAME . '/languages/' . $domain . '-' . $locale . '.mo';
         }
@@ -21,15 +21,15 @@ class TextDomain
 
     public static function __($text, ...$args) {
         if (empty($args)) {
-            return __($text, TSM__TEXT_DOMAIN);
+            return __($text, "traduire-sans-migraine");
         }
-        return sprintf(__($text, TSM__TEXT_DOMAIN), ...$args);
+        return sprintf(__($text, "traduire-sans-migraine"), ...$args);
     }
 
     public static function _n($single, $plural, $number, ...$args) {
         if (empty($args)) {
-            return _n($single, $plural, $number, TSM__TEXT_DOMAIN);
+            return _n($single, $plural, $number, "traduire-sans-migraine");
         }
-        return sprintf(_n($single, $plural, $number, TSM__TEXT_DOMAIN), ...$args);
+        return sprintf(_n($single, $plural, $number, "traduire-sans-migraine"), ...$args);
     }
 }
