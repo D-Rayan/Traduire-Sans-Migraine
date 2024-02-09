2518d11bd4ab0e86d5bd0426db84dcea776d886f
Rayan <rayan.djebli@hotmail.fr>
2024-02-09 15:09:18 +0100

    fix

--- cli/updatingVersion.php
+++ cli/updatingVersion.php
@@ -1,6 +1,6 @@
 <?php
 
-include "../env.php";
+include "../env.global.php";
 $fileContent = file_get_contents(__DIR__ . "/../traduire-sans-migraine.php");
 $fileContent = str_replace("PLACEHOLDER_VERSION", TSM__VERSION, $fileContent);
 file_put_contents(__DIR__ . "/../traduire-sans-migraine.php", $fileContent);
