879f539cfab9e47da70dd79d118daec49494853d
Rayan <rayan.djebli@hotmail.fr>
2024-03-21 10:55:03 +0100

    add translations

--- env.production.php
+++ env.production.php
@@ -2,5 +2,6 @@
 
 define("TSM__URL_DOMAIN", "https://www.seo-sans-migraine.fr");
 define("TSM__API_DOMAIN", "https://traduire-sans-migraine.seo-sans-migraine.fr/api");
+define("TSM__CLIENT_LOGIN_DOMAIN", "https://client.seo-sans-migraine.fr");
 
 include "env.global.php";
--- env.staging.php
+++ env.staging.php
@@ -2,5 +2,6 @@
 
 define("TSM__URL_DOMAIN", "https://staging.seo-sans-migraine.fr");
 define("TSM__API_DOMAIN", "https://traduire-sans-migraine-staging.seo-sans-migraine.fr/api");
+define("TSM__CLIENT_LOGIN_DOMAIN", "https://client-staging.seo-sans-migraine.fr");
 
 include "env.global.php";
