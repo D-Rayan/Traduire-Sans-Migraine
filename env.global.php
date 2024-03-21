5d9033c11966ed78a0e688f5690241fc39ffd0b4
Rayan <rayan.djebli@hotmail.fr>
2024-03-21 11:31:03 +0100

    add translations

--- env.global.php
+++ env.global.php
@@ -1,6 +1,6 @@
 <?php
 
-define("TSM__VERSION", "0.4.203");
+define("TSM__VERSION", "0.4.204");
 define("TSM__WORDPRESS_TESTED", "5.8");
 define("TSM__PHP_REQUIREMENT", "7.0");
 define("TSM__WORDPRESS_REQUIREMENT", "6.4");
