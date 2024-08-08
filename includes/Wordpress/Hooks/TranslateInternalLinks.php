c4a0e2c845707457fde1cdd7a090d0fd1ff4f2b0
Rayan <rayan.djebli@hotmail.fr>
2024-08-08 14:27:01 +0200

    Better handling translateInternalLinks for elementor

--- includes/Wordpress/Hooks/TranslateInternalLinks.php
+++ includes/Wordpress/Hooks/TranslateInternalLinks.php
@@ -40,6 +40,36 @@ class TranslateInternalLinks {
         $this->loadHooks();
     }
 
+    private function handleElementor($postId, $codeTo, $codeFrom) {
+        if (is_plugin_active("elementor/elementor.php")) {
+            $postMetas = get_post_meta($postId);
+            foreach ($postMetas as $key => $value) {
+                if (strstr($key, "elementor")) {
+                    $valueKey = $value[0];
+                    if ($this->is_serialized($valueKey)) {
+                        continue;
+                    }
+                    $newValueKey = $this->linkManager->translateInternalLinks($valueKey, $codeFrom, $codeTo);
+                    if ($newValueKey !== $valueKey) {
+                        if ($this->is_json($newValueKey)) {
+                            $newValueKey = wp_slash($newValueKey);
+                        }
+                        update_post_meta($postId, $key, $newValueKey);
+                    }
+                }
+            }
+        }
+    }
+
+    private function is_json($string) {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+
+    private function is_serialized($string) {
+        return ($string == serialize(false) || @unserialize($string) !== false);
+    }
+
     public function translateInternalLinks() {
         if (!isset($_GET["wp_nonce"])  || !wp_verify_nonce($_GET["wp_nonce"], "traduire-sans-migraine_editor_translate_internal_links")) {
             wp_send_json_error([
@@ -66,6 +96,7 @@ class TranslateInternalLinks {
                 continue;
             }
             $content = $this->linkManager->translateInternalLinks($content, $slug, $language);
+            $this->handleElementor($post->ID, $slug, $language);
         }
         wp_update_post([
             "ID" => $post->ID,
